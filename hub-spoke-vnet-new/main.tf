241af1495a0a1ed8f3d67b014f205839efcf67db
Anil <anil.shafeeque@gmail.com>
2019-10-16 18:44:57 +0000

    changed the nsg name

--- hub-spoke-vnet-new/main.tf
+++ hub-spoke-vnet-new/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_virtual_network" "spoke" {
 }
   
 resource "azurerm_network_security_group" "spoke" {
-  name                = "dremio-nsg"
+  name                = "mgmtsubnet-nsg"
   location            = "${azurerm_resource_group.vnet_gp.location}"
   resource_group_name = "${azurerm_resource_group.vnet_gp.name}"
   tags = "${merge( local.common_tags)}"
